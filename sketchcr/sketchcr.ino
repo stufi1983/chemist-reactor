788d989524390f1b7e92b34c30fac2f87c518299
stufi1983 <stufi1983@gmail.com>
2015-10-06 16:35:38 +0700

    Testing minimum system

--- sketchcr/sketchcr.ino
+++ sketchcr/sketchcr.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 13; // Analog output pin that the LED is attached to
 
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(analogInPin);
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
+  analogWrite(analogOutPin, outputValue);
 
+  Serial.print(F("sensor = " ));
+  Serial.println(sensorValue);
+
+  delay(1000);
+  analogWrite(analogOutPin, 0);
+  delay(1000);
 }
